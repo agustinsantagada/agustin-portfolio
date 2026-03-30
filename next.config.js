11a7dc5ad883c4ff2020907a6bbf5a13b29aa3c2
Agustin Santagada <agustinsantagada@gmail.com>
2026-03-30 15:54:27 +0200

    Local images with full project content

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'images.squarespace-cdn.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'player.vimeo.com',
+      },
     ],
   },
 }
